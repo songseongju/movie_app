b2d6d278674b350ce3147f0f30fa5aed1d76642d
송성주 <122763566+songseongju@users.noreply.github.com>
2023-08-30 09:46:41 +0900

    Add files via upload
    app v1.0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
 
-function Food(props){
-
-  console.log(props);
-  return <h1>I like {props.fav} </h1>
-}
-
-
-function App() {
-  return <div>MULTI Movie World!!!!
-
-
-   <Food fav="kimchi" />
-
-
-  </div>;
-
-  
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div class="movies">
+            {movies.map((movie) => {
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              );
+            })}
+          </div>
+        )}
+      </section>
+    );
+  }
 }
 
 export default App;
-
-
